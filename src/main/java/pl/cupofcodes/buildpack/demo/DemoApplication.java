9e1088488cac0e1040c4113814fd8047d328c433
softko <sebastian.kozak@outlook.com>
2021-08-07 11:25:39 +0200

    initial commit

---
+++ src/main/java/pl/cupofcodes/buildpack/demo/DemoApplication.java
@@ -0,0 +1,14 @@
+package pl.cupofcodes.buildpack.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		String classa1 = "as";
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
